8eb2f6ad1e5d2bac8cf505e8ef63e3f321f87eff
Josh Thomas <jthoms1@gmail.com>
2017-11-11 14:32:37 -0600

    added baseline code example.

---
+++ 1_baseline/index.js
@@ -0,0 +1,20 @@
+var path = require('path');
+var fs = require('fs');
+var dirPath = process.argv[2];
+if (!dirPath) {
+    throw new Error('A directory path was not provided.');
+}
+var fullPath = path.resolve(__dirname, dirPath);
+var files = fs.readdirSync(fullPath);
+files.sort();
+console.log('Contents of directory ' + fullPath);
+for (var i = 0; i < files.length; i++) {
+    var fileName = files[i];
+    var isHidden = fileName.charAt[0] === '.';
+    var filePath = path.join(dirPath, fileName);
+    var fileStat = fs.statSync(filePath);
+    var isDirectory = fileStat.isDirectory();
+    var messageLine = (isDirectory ? "d" : '-') + " " +
+        files[i];
+    console.log(messageLine);
+}
