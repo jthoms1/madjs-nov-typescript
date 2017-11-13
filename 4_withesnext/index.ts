130e59bc053787f2a3993f0675e51f63ec6a3abd
Josh Thomas <jthoms1@gmail.com>
2017-11-12 22:13:53 -0600

    make example a little simpler.

--- 4_withesnext/index.ts
+++ 4_withesnext/index.ts
@@ -10,12 +10,10 @@ const fullDirPath = path.resolve(dirPath);
 
 console.log('Contents of directory ' + fullDirPath);
 
-(async () => {
-  await forEachFileInDir(fullDirPath, async (filePath) => {
-    const { isHidden, isDirectory, pathData } = await getFileData(filePath);
-    if (isHidden) {
-      return;
-    }
-    console.log(`${isDirectory ? 'd' : '-'} ${pathData.base}`);
-  });
-})();
+forEachFileInDir(fullDirPath, async (filePath) => {
+  const { isHidden, isDirectory, pathData } = await getFileData(filePath);
+  if (isHidden) {
+    return;
+  }
+  console.log(`${isDirectory ? 'd' : '-'} ${pathData.base}`);
+});
